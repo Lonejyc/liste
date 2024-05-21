86f9edc7b4b5ee04fcf1d73f3924e257695f85e9
Raph <raphael@nouvel-oeil.com>
2024-05-21 16:15:14 +0200

    Layout signin-signout

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from './Layout.module.css'; // Assurez-vous de créer ce fichier CSS
+import { signIn, useSession } from 'next-auth/react';
 
 const Layout = ({ children, title = 'Default Title' }) => {
+
+    const { data: session, status } = useSession();
+    let isAuthenticated = false;
+    if (status === "authenticated") {
+        isAuthenticated = true;
+    }
+
     return (
         <div className={styles.layout}>
             <Head>
@@ -11,10 +19,14 @@ const Layout = ({ children, title = 'Default Title' }) => {
                 <meta name="description" content="Une application Next.js bien structurée" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header className={styles.header}>
+            <header className="flex flex-row p-3 bg-blue-500">
                 <nav className="flex items-center gap-6 text-sm">
                     <Link href="/">Home</Link>
-                    <Link href="/liste">Projects</Link>
+                    {isAuthenticated && <Link href="/liste">Projects</Link>}
+                </nav>
+                <nav className='flex items-center gap-6 text-sm'>
+                    {!isAuthenticated && <button onClick={() => signIn()}>Sign In</button>}
+                    {isAuthenticated && <button onClick={() => signOut()}>Sign Out</button>}
                 </nav>
             </header>
             <main className="flex flex-col items-center justify-center gap-3 px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
--- pages/liste/index.js
+++ pages/liste/index.js
@@ -3,21 +3,24 @@ import { getSession } from "next-auth/react";
 import fs from 'fs';
 import path from 'path';
 import Link from "next/link";
+import Layout from '../../components/Layout';
 
 export default function Liste({ session, folders }) {
   if (!session) return <p>Access Denied</p>;
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Project List</h1>
-      <ul className="list-disc pl-4">
-        {folders.map((folder) => (
-          <li key={folder} className="mb-2">
-            <Link href={`localhost:3000/${folder}`} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">{folder}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Layout>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-2xl font-bold mb-4">Project List</h1>
+        <ul className="list-disc pl-4">
+          {folders.map((folder) => (
+            <li key={folder} className="mb-2">
+              <Link href={`localhost:3000/${folder}`} className="text-blue-500 hover:underline">{folder}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
   );
 }
 
