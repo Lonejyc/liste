552879e508e743292b496622fa976d52ae199e85
lonejyc <jocelyn.marcilloux@gmail.com>
2026-01-13 10:50:47 +0100

    chore: change to local url

--- pages/api/system-info.js
+++ pages/api/system-info.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://monitor-agent.jocelynmarcilloux.com/api/stats');
+    const response = await fetch('http://monitor-agent:5000/api/stats');
     const data = await response.json();
       res.status(200).json(data);
     } catch (error) {
